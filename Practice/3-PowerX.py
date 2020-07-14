c7f3584d024cfe5a05e0b8e0bb794112b724586b
Dll <dengle2904158746@gmail.com>
2020-07-14 23:53:14 +0800

    堆

--- Practice/3-PowerX.py
+++ Practice/3-PowerX.py
@@ -13,17 +13,32 @@
     # 递归计算幂方，二分优化，2^6 = 2^3*2^3,故可以二分求幂
 # 思路二：
     # 位运算，把n转成二进制，如n=9，——>1001，求m的9次方，
-    # 即m^9 = m^1000 * m^0001
+    # 即m^9 = m^1000 * m^0001，所以只需要把是1的乘数累积到结果中
+    # 如2^9，拆分为1000,0001（8,1）
+
 
 def powerX1(x, n):
     if n == 0:
         return 1
     else:
-        temp = powerX(x, n//2)
+        temp = powerX1(x, n//2)
         result = temp * temp
         if n % 2 == 1:
             result *= x
         return result
 
+def powerX2(x, n):
+    result = 1
+    temp = x
+    while n != 0:
+        if n & 1 == 1:
+            result *= temp
+        temp *= temp
+        n >>= 1
+    return result
 
-print(powerX1(2, 5))
+
+if __name__ == '__main__':
+    
+    print(powerX1(2, 5))
+    print(powerX2(2, 10))
